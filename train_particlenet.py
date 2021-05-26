567ddf263da947f88c3c5ac1c989dc575e792e14
Thomas on GCloud <tklijnsm@gmail.com>
2021-05-26 15:32:14 +0000

    trying to get particlenet to work

--- train_particlenet.py
+++ train_particlenet.py
@@ -14,24 +14,33 @@ from sklearn.metrics import confusion_matrix
 
 import tensorflow.keras as keras
 
+import tensorflow_addons as tfa
+
 
 def main():
-    X_train, y_train = bbefp.dataset.get_data_particlenet('data/train/merged.npz', N=200)
-    X_test, y_test = bbefp.dataset.get_data_particlenet('data/test/merged.npz', N=200)
+    N = 100 # Number of constituents per jet
 
-    ckpt_dir = strftime('testckpts_particlenet_%b%d_%H%M%S')
+    X_train, y_train = bbefp.dataset.get_data_particlenet('data/train/merged.npz', N=N)
+    X_test, y_test = bbefp.dataset.get_data_particlenet('data/test/merged.npz', N=N)
 
-    model = bbefp.particlenet.get_particle_net(
+
+    print('train:')
+    print(X_train['points'].shape, X_train['features'].shape, X_train['mask'].shape, y_train.shape)
+    print('test:')
+    print(X_test['points'].shape, X_test['features'].shape, X_test['mask'].shape, y_test.shape)
+
+
+    model = bbefp.particlenet.get_particle_net_lite(
         num_classes=2,
         input_shapes=dict(
-            points=(200, 2),
-            features=(200, 5),
-            mask=(200,1)
+            points=(N, 2),
+            features=(N, 5),
+            mask=(N,1)
             )
         )
 
     def lr_schedule(epoch):
-        lr = 1e-3
+        lr = 1e-4
         if epoch > 10:
             lr *= 0.1
         elif epoch > 20:
@@ -39,9 +48,26 @@ def main():
         logging.info('Learning rate: %f'%lr)
         return lr
 
+    # Prepare callbacks for model saving and for learning rate adjustment.
+    ckpt_dir = strftime('testckpts_particlenet_%b%d_%H%M%S')
+    checkpoint = keras.callbacks.ModelCheckpoint(
+        filepath=ckpt_dir,
+        monitor='val_accuracy', # Or 'val_acc', depending on versions (https://github.com/tensorflow/tensorflow/issues/33163)
+        verbose=1,
+        save_best_only=True
+        )
+
+    lr_scheduler = keras.callbacks.LearningRateScheduler(lr_schedule)
+    # progress_bar = keras.callbacks.ProgbarLogger()
+    callbacks = [
+        checkpoint, lr_scheduler,
+        # progress_bar
+        ]
+
     model.compile(
         loss='categorical_crossentropy',
-        optimizer=keras.optimizers.Adam(learning_rate=lr_schedule(0)),
+        # optimizer=keras.optimizers.Adam(learning_rate=lr_schedule(0)),
+        optimizer=tfa.optimizers.AdamW(weight_decay=.001, learning_rate=lr_schedule(0)),
         metrics=['accuracy']
         )
     model.summary()
@@ -50,11 +76,11 @@ def main():
 
     model.fit(
         X_train, y_train,
-        batch_size=16,
+        batch_size=32,
         epochs=20, # --- train only for 1 epoch here for demonstration ---
         validation_data=(X_test, y_test),
         shuffle=True,
-        # callbacks=callbacks
+        callbacks=callbacks
         )
 
 if __name__ == '__main__':
